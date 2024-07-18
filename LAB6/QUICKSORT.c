2c401e1ac327d2598fbc5e23f1d978507868dee8
nithinkoushik <153620277+nithinkoushik@users.noreply.github.com>
2024-07-18 14:52:37 +0530

    Update QUICKSORT.c

--- LAB6/QUICKSORT.c
+++ LAB6/QUICKSORT.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <time.h>
 
 void swap(int* a, int* b) {
     int temp = *a;
@@ -6,85 +7,70 @@ void swap(int* a, int* b) {
     *b = temp;
 }
 
-void quicksort(int a[],int low, int high)
-{
-    int split;
-    if(low<high)
-    {
-        split=partition(a,low,high);
-        quicksort(a,low,split-1);
-        quicksort(a,split+1,high);
-    }
-
-}
-
-int partition(int a[],int low,int high)
-{
-    int pivot,i,j;
-    pivot=a[low];
-    i=low+1;
-    j=high;
-    do{
-        while(a[i]>=pivot){
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
+    
+    while (1) {
+        while (i <= high && arr[i] <= pivot) {
             i++;
         }
-        while(a[j]<=pivot){
+        while (j >= low && arr[j] > pivot) {
             j--;
         }
-        swap(&a[i],&a[j]);
-    }while(i>=j);
-    swap(&a[i],&a[j]);
-    swap(&a[j],&a[low]);
-    return j;
+        if (i < j) {
+            swap(&arr[i], &arr[j]);
+        } else {
+            arr[low] = arr[j];
+            arr[j] = pivot;
+            return j;
+        }
+    }
 }
 
-void main()
-{
-int a[15000],n, i,j,ch, temp;
-   clock_t start,end;
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
 
-   while(1)
-   {
- printf("\n1:For manual entry of N value and array elements");
- printf("\n2:To display time taken for sorting number of elements N in the range 500 to 14500");
- printf("\n3:To exit");
-     printf("\nEnter your choice:");
-     scanf("%d", &ch);
-     switch(ch)
-     {
-       case 1:  printf("\nEnter the number of elements: ");
-		scanf("%d",&n);
-		printf("\nEnter array elements: ");
-		for(i=0;i<n;i++)
-		 {
-		  scanf("%d",&a[i]);
-		 }
-		start=clock();
-		quicksort(a,0,n-1);
-		end=clock();
-		printf("\nSorted array is: ");
-		for(i=0;i<n;i++)
-		printf("%d\t",a[i]);
-printf("\n Time taken to sort %d numbers is %f Secs",n, (((double)(end-start))/CLOCKS_PER_SEC));
-		break;
-     case 2:
-	       n=500;
-	       while(n<=14500) {
-	       for(i=0;i<n;i++)
-		  {
+int main() {
+    int a[15000], n, i, ch;
+    clock_t start, end;
 
-		    a[i]=n-i;
-		  }
-	       start=clock();
-	       quicksort(a,0,n-1);
-	   for(j=0;j<500000;j++){ temp=38/600;}
-	       end=clock();
-printf("\n Time taken to sort %d numbers is %f Secs",n, (((double)(end-start))/CLOCKS_PER_SEC));
-	             n=n+1000;
-		  }
-	      break;
-   case 3: exit(0);
-   }
-   getchar();
+    while (1) {
+        printf("\n1: For manual entry of N value and array elements");
+        printf("\n2: Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1:  
+                printf("\nEnter the number of elements: ");
+                scanf("%d", &n);
+                printf("\nEnter array elements: ");
+                for (i = 0; i < n; i++) {
+                    scanf("%d", &a[i]);
+                }
+                start = clock();
+                quicksort(a, 0, n - 1);
+                end = clock();
+                printf("\nSorted array is: ");
+                for (i = 0; i < n; i++) {
+                    printf("%d\t", a[i]);
+                }
+                printf("\nTime taken to sort %d numbers is %f Secs", n, (((double)(end - start)) / CLOCKS_PER_SEC));
+                break;
+
+            case 2:
+                return 0;
+
+            default:
+                printf("Invalid choice, please try again.");
+        }
+        getchar(); // To consume the newline character left by scanf
     }
 }
